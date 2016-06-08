07cb6ac3efe676909095fc5955eaea3229cceb2e
Cendrzak, Wojciech (FP) <wcendrzak@future-processing.com>
2016-06-08 09:46:58 +0200

    initial

---
+++ AzureInputValidationTest/AzureInputValidationTest/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace AzureInputValidationTest.Controllers
+{
+	public class HomeController: Controller
+	{
+		// 1
+		[ValidateInput(false)]
+		public ActionResult Index(string myInput)
+		{
+			ViewBag.MyInputValue = myInput;
+			return View();
+		}
+	}
+}
